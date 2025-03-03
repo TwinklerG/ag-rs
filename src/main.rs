b22bdd1e9008220d4a086fd3e9ba73e6f7e5feda
TwinklerG <231830131@smail.nju.edu.cn>
2025-03-03 11:15:49 +0800

    optimize Chinese input experience

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use clap::Parser;
 use futures_util::StreamExt;
 use reqwest::header::HeaderMap;
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::time::Duration;
 use termion::color;
 
 const URL: &str = "https://api.siliconflow.cn/v1/chat/completions";
-const MODEL: &str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B";
 
 /// Config for your ag
 #[derive(Parser)]
@@ -19,7 +20,9 @@ struct Cli {
 
     /// Choice of your model. DeepSeek-R1-Distill-Llama-8B default, which is free.
     /// r1: deepseek-ai/DeepSeek-R1
-    #[arg(long)]
+    /// q2_5-7: Qwen2.5-7B-Instruct
+    /// default: deepseek-ai/DeepSeek-R1-Distill-Llama-8B
+    #[arg(long, verbatim_doc_comment)]
     model: Option<String>,
 }
 
@@ -49,6 +52,8 @@ struct Chunk {
 #[derive(Serialize, Deserialize, Debug)]
 struct Usage {
     total_tokens: usize,
+    prompt_tokens: usize,
+    completion_tokens: usize,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -62,44 +67,70 @@ struct Delta {
     reasoning_content: Option<String>,
 }
 
+macro_rules! model_map {
+    ($var:expr) => {
+        match $var {
+            "r1" => Ok("deepseek-ai/DeepSeek-R1"),
+            "q2_5-7" => Ok("Qwen/Qwen2-7B-Instruct"),
+            "ds-8" => Ok("deepseek-ai/DeepSeek-R1-Distill-Llama-8B"),
+            _ => Err("deepseek-ai/DeepSeek-R1-Distill-Llama-8B"),
+        }
+    };
+}
+
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
     let api_key = std::env::var("API_KEY").expect("You must set the API_KEY environment variable");
     let client = reqwest::Client::new();
-    let mut payload = Payload::new(
-        if let Some(model) = cli.model {
-            if &model == "r1" {
-                "deepseek-ai/DeepSeek-R1".into()
-            } else {
-                panic!("Unknown Model");
-            }
-        } else {
-            MODEL.into()
-        },
-        true,
-    );
+    let model = if let Some(model) = cli.model {
+        model
+    } else {
+        "ds-8".to_string()
+    };
+    let model: Result<&str, &str> = model_map!(&model[..]);
+    let model = match model {
+        Ok(model) => {
+            println!("{}", model);
+            model.to_string()
+        }
+        Err(model) => {
+            println!("Unknown model. Using {} by default", model);
+            model.to_string()
+        }
+    };
+    let mut payload = Payload::new(model.clone(), true);
+    let mut rl = DefaultEditor::new().unwrap();
     loop {
-        print!("☁️ : ");
-        std::io::stdout().flush().unwrap();
-        let mut s = String::new();
-        if let Err(error) = {
-            if cli.multi_lines {
-                std::io::stdin().read_to_string(&mut s)
-            } else {
-                std::io::stdin().read_line(&mut s)
+        let mut input = String::new();
+        let mut readline = rl.readline("☁️ : ");
+        loop {
+            match &readline {
+                Ok(line) => {
+                    input.push_str(line);
+                }
+                Err(ReadlineError::Interrupted) => {
+                    eprintln!("Terminated with CTRL-C");
+                    return;
+                }
+                Err(ReadlineError::Eof) => break,
+                Err(err) => {
+                    println!("Error: {:?}", err);
+                    break;
+                }
             }
-        } {
-            println!("❌ Read input error: {}", error);
-            continue;
-        };
-        let s = s.trim();
-        if s == "q" || s.is_empty() {
+            if !cli.multi_lines {
+                break;
+            }
+            readline = rl.readline("");
+        }
+        let input = input.trim();
+        if input == "q" || input.is_empty() {
             break;
         }
         let mut tmp = HashMap::new();
         tmp.insert("role".to_string(), "user".to_string());
-        tmp.insert("content".to_string(), s.to_string());
+        tmp.insert("content".to_string(), input.to_string());
         payload.messages.push(tmp);
         let mut headers = HeaderMap::new();
         headers.insert(
@@ -116,21 +147,21 @@ async fn main() {
             .await
             .expect("Failed to send request. Check your Internet connection")
             .bytes_stream();
-        print!(
-            "🚀 {} 深度思考中...\n{}<think>\n",
-            payload.model,
-            color::Fg(color::Rgb(200, 200, 200))
-        );
-        let mut think = true;
+        let mut think = false;
         std::io::stdout().flush().unwrap();
-        let mut tokens = 0;
+        let mut usage = Usage {
+            total_tokens: 0,
+            prompt_tokens: 0,
+            completion_tokens: 0,
+        };
+        println!("🚀 {}", &model);
         while let Some(chunk) = stream.next().await {
             if chunk.is_err() {
                 continue;
             }
             let chunk = chunk.unwrap().slice(6..);
             if let Ok(chunk) = serde_json::from_slice::<Chunk>(&chunk) {
-                tokens = chunk.usage.total_tokens;
+                usage = chunk.usage;
                 if let Some(content) = chunk.choices[0].clone().delta.content {
                     if think {
                         println!("\n</think>\n{}", color::Fg(color::Reset));
@@ -141,11 +172,22 @@ async fn main() {
                 } else if let Some(reasoning_content) =
                     chunk.choices[0].clone().delta.reasoning_content
                 {
+                    if !think {
+                        think = true;
+                        println!("{}<think>", color::Fg(color::Rgb(200, 200, 200)));
+                    }
                     print!("{}", reasoning_content);
                     std::io::stdout().flush().unwrap();
                 }
             }
         }
-        println!("\n{}total tokens: {}{}", color::Fg(color::Yellow),tokens, color::Fg(color::Reset));
+        println!(
+            "\n{}total tokens: {} = {} + {}{}",
+            color::Fg(color::Yellow),
+            usage.total_tokens,
+            usage.prompt_tokens,
+            usage.completion_tokens,
+            color::Fg(color::Reset)
+        );
     }
 }
